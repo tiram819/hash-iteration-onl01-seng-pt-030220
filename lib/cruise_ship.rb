54690f8c14990364699c5313b73a82e0107a49f1
Tabassum Hassan <tiram819@gmail.com>
2020-03-13 19:29:35 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -20,7 +20,7 @@ end
 
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
+  birthday_kids.each do |kids_name,age|
     puts "Happy Birthday #{kids_name}! 
    You are now #{age} years old!"
   end
