42fddf1c24035190e9530c96d560c6ce7b00c019
Tabassum Hassan <tiram819@gmail.com>
2020-03-13 20:58:44 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,10 +15,11 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |name,age|
     if 12 >= age
       puts "Happy Birthday #{name}! You are now #{age} years old!"
+    end
       else puts "You are too old for this."
   end
 end
-end
+
 
 
 
